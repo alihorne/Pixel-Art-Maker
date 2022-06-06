ed5bce73fbd5a29d59c74940284b4c71ed5976f5
Ashley"Ali" Horne <102095588+alihorne@users.noreply.github.com>
2022-06-06 03:08:22 -0400

    Update PixelArtMaker1.js

--- JavaScript/PixelArtMaker1.js
+++ JavaScript/PixelArtMaker1.js
@@ -2,36 +2,34 @@
 // Select height/width input
 let picker = document.getElementById('sizePicker');
 let color = document.getElementById('colorPicker');
-const Canvas  = document.getElementById('pixelCanvas');
+const Canvas = document.getElementById('pixelCanvas');
 
 const height = document.getElementById('inputHeight').value;
 const width = document.getElementById('inputWidth').value;
 
 
 // Size is submitted, call makeGrid()
-picker.addEventListener('submit', function(event){
-    const height = document.getElementById('inputHeight').value; 
-    const width = document.getElementById('inputWidth').value; 
-    const Canvas = document.getElementById('pixelCanvas'); 
-    event.preventDefault(); 
+picker.addEventListener('submit', function (event) {
+    Canvas.innerHTML = "";
+    event.preventDefault();
     makeGrid(height, width)
-}); 
+});
 
 function makeGrid() {
     let height = inputHeight.value
     let width = inputWidth.value
-  
+
     for (let a = 1; a <= height; a++) {
-      var row = document.createElement('tr') ;
-      for (let m = 1; m <= width; m ++) {
-        var column = document.createElement('td');
-        column.id = "column-i-j";
-        row.appendChild(column);
-        Canvas.appendChild(row);
-      }
+        var row = document.createElement('tr');
+        for (let m = 1; m <= width; m++) {
+            var column = document.createElement('td');
+            column.id = "column-i-j";
+            row.appendChild(column);
+            Canvas.appendChild(row);
+        }
     }
-} 
+}
 
 Canvas.addEventListener("click", function (event) {
     event.target.style.backgroundColor = colorPicker.value;
-  });
+});
